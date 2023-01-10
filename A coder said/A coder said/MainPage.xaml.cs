e7cdaa19298f587fcd734c58d5367e885800ac49
MadLies <jwilchesc@unal.edu.co>
2023-01-10 18:05:43 -0500

    Change the background

--- A coder said/A coder said/MainPage.xaml.cs
+++ A coder said/A coder said/MainPage.xaml.cs
@@ -1,14 +1,34 @@
-﻿namespace A_coder_said;
+﻿
+
+namespace A_coder_said;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	Random random = new Random();
 
+    private void btnGenerate_Clicked(object sender, EventArgs e)
+    {
+		var startColor = System.Drawing.Color.FromArgb(random.Next(0,256), random.Next(0, 256), random.Next(0, 256));
+        var EndColor = System.Drawing.Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        var colors = ColorUtility.ColorControls.GetColorGradient(startColor, EndColor,6);
 
+		float stopOffset = .0f;
+		var stops = new GradientStopCollection();
+		foreach (var c in colors)
+		{
+			stops.Add(new GradientStop(Color.FromArgb(c.Name), stopOffset));
+			stopOffset += .2f;
+		}
+		var gradient = new  LinearGradientBrush(stops , new Point(0,0)
+														, new Point(0,1));
+
+		background.Background= gradient;
+
+    }
 }
 
